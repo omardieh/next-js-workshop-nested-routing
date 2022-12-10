a73fee63341fb610af3ea539ad743b83570b9a2f
omardiehs <omar.dieh@wildcodeschool.com>
2022-12-10 19:58:49 +0100

    fixed route

--- pages/api/users/[user_id]/index.js
+++ pages/api/users/[user_id]/index.js
@@ -1,5 +1,5 @@
 import users from "/database.json";
 
 export default function handler(req, res) {
-  res.status(200).json(users);
+  res.json(users.find((user) => user.user_id === +req.query.user_id));
 }
