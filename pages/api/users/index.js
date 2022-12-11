e9719d2fc49736e9c03782eeb13501030391c418
omardiehs <omar.dieh@wildcodeschool.com>
2022-12-11 15:29:44 +0100

    SSR example in users page

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,5 +1,11 @@
 import users from "/database.json";
 
-export default function handler(req, res) {
-  res.status(200).json(users);
+export async function getUsers() {
+  const data = await users;
+  return data;
+}
+
+export default async function handler(req, res) {
+  const data = await getUsers();
+  res.status(200).json(data);
 }
